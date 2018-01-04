50b69306e32d52f54b0bd07c34b8e3ee3bb90407
Ferdinand Enario <ftenario@yahoo.com>
2018-01-03 23:08:41 -0800

    initial commit

---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.AspNetCore.Mvc;
+using Vidly.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Random()
+        {
+            var movie = new Movie() { Name = "Shrek!" };
+            //return View(movie);
+            //return Content("Hello World");
+            return StatusCode(404);
+            //return RedirectToAction("Index", "Home");
+        }
+    }
+}
